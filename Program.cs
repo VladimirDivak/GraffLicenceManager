be8e6810dccc66ddde2c0d442f23a5fa517a355a
vladimir-d <vladimir.divak@graff.tech>
2022-12-26 18:07:13 +0500

    migration to .NET 6

--- Program.cs
+++ Program.cs
@@ -1,30 +1,56 @@
+using GraffLicenceManager;
+using GraffLicenceManager.DB;
+using GraffLicenceManager.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
 using System.Net;
 
-namespace GraffLicenceManager
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddResponseCompression(opts =>
 {
-    public enum CardPosition
-    {
-        Top,
-        Center,
-        Bottom,
-        Full
-    }
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+builder.Services.AddSingleton<DatabaseService>();
+builder.Services.AddSingleton<MailSender>();
+builder.Services.AddScoped(x =>
+{
+    var navManager = x.GetRequiredService<NavigationManager>();
+    return new HubConnectionBuilder()
+        .WithUrl(navManager.ToAbsoluteUri("/adminhub"))
+        .Build();
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseKestrel(x => x.Listen(IPAddress.Parse("0.0.0.0"), 5000));
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+app.UseResponseCompression();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+    endpoints.MapHub<UserHub>("/userhub");
+    endpoints.MapHub<AdminHub>("/adminhub");
+});
+
+app.Run();
+
+
+public enum CardPosition
+{
+    Top,
+    Center,
+    Bottom,
+    Full
 }
